85d728cddef6d109c574f3c9d24eb54c187e75fb
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-11-11 17:08:18 +0530

    Implemented some functionalites of ShortenURL function in shorten and helper file

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -18,4 +18,29 @@ type response struct {
 }
 
 
+func ShortenURL(c *fiber.ctx)  error{
+
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	}
+
+	//implement rate limiting
+	
+
+	//check if the input is actual URL or not
+	if !govalidator.IsURL(body.URL){
+		return c.status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//check for domain error
+	if !helpers.RemoveDomainError(body.URL){
+		return c.status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": "Invalid Domain Error"})
+	}
+
+	//enforce https, SSL
+	body.URL=helpers.EnforceHTTP(body.URL)
+} 
+
+
 
