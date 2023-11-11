22acbe9184ab527a3648a79dfd1233816656ee15
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-11-11 18:19:46 +0530

    Implemented Rate Limiting in the shorten.go file

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,8 @@ package routes
 
 import(
 	"time"
+	"github.com/AdityaNarayan05/shorten-url/database"
+	"os"
 )
 type request struct {
 	URL                string              'json:"url"'
@@ -26,7 +28,24 @@ func ShortenURL(c *fiber.ctx)  error{
 	}
 
 	//implement rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val,err := r2.Get(database.Ctx,c.IP()).Result()
+
+	if err==redis.Nil{
+		_=r2.Set(database.Ctx,c.IP,os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val,_ := r2.Get(database.Ctx,c.IP().Result())
+		valInt,_ := strconv.Atoi(val)
 	
+		if valInt <= 0{
+			limit,_ :=r2.TTL(database.Ctx,c.IP()).Result()
+			return c.status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":"Rate Limit exceeded",
+				"rate_limit_reset":limit / time.timeNanosecond/time.Minute,
+			})
+		}
+	}
 
 	//check if the input is actual URL or not
 	if !govalidator.IsURL(body.URL){
@@ -40,6 +59,8 @@ func ShortenURL(c *fiber.ctx)  error{
 
 	//enforce https, SSL
 	body.URL=helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx,c.IP());
 } 
 
 
